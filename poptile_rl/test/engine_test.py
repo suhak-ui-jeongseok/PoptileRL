00f2cecf96e5bec0757607f27ec7ed768674f035
Uniaut <kunwoo0927@gmail.com>
2022-03-25 22:19:44 +0900

    environment.engine test code

---
+++ poptile_rl/test/engine_test.py
@@ -0,0 +1,23 @@
+from ..environment.engine import NewGame
+
+
+if __name__ == '__main__':
+    game = NewGame(3, 10, 10)
+    game.generate_row()
+    game.generate_row()
+    game.generate_row()
+    game.generate_row()
+    game.generate_row()
+    game.generate_row()
+    game.generate_row()
+    game.generate_row()
+
+    n2c = ['0', '1', '2', ' ']
+    print('before')
+    for r in reversed(game.board.state):
+        print(*map(lambda i: n2c[i], r))
+    
+    game.pop_tile(0, 0)
+    print('after')
+    for r in reversed(game.board.state):
+        print(*map(lambda i: n2c[i], r))

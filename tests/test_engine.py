a6328f9ecb4168f4cdc6163c92053c4bb3b72f10
Uniaut <kunwoo0927@gmail.com>
2022-03-27 18:55:48 +0900

    [Test] Test of gameover state in Engine

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,22 +1,20 @@
 from poptile_rl.environment.engine import NewGame
 
-if __name__ == '__main__':
-    game = NewGame(3, 10, 10)
-    game.generate_row()
-    game.generate_row()
-    game.generate_row()
-    game.generate_row()
-    game.generate_row()
-    game.generate_row()
-    game.generate_row()
-    game.generate_row()
 
+def print_game(data):
     n2c = ['0', '1', '2', ' ']
-    print('before')
-    for r in reversed(game.board.state):
+    for r in reversed(data):
         print(*map(lambda i: n2c[i], r))
 
-    game.pop_tile(0, 0)
-    print('after')
-    for r in reversed(game.board.state):
-        print(*map(lambda i: n2c[i], r))
+
+if __name__ == '__main__':
+    game = NewGame(3, 15, 8)
+    game.generate_row()
+    for i in range(1000):
+        print(f'State {i}:')
+        print_game(game.board.state_as_array())
+        game.pop_tile(0, i % 8)
+        if game.is_gameover():
+            break
+    
+    print(f'score: {game.score}')

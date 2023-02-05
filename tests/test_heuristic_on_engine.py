e879ed6522610ebc10840b0447a0cb89f49d712c
Uniaut <kunwoo0927@gmail.com>
2023-02-05 16:56:19 +0900

    [Update] Trivial: on test heuristic on engine

--- tests/test_heuristic_on_engine.py
+++ tests/test_heuristic_on_engine.py
@@ -1,12 +1,12 @@
 import time
 
-from poptile_rl.environment.engine import NewGame
+from poptile_rl.environment.engine import Engine, NewGame
 from poptile_rl.model.heuristic_baseline import search_best
 
 
-def print_game(engine):
+def print_game(engine: Engine):
     n2c = ['0', '1', '2', ' ']
-    for r in reversed(game.board.state):
+    for r in reversed(engine.board.state):
         print(*map(lambda i: n2c[i], r))
 
 
